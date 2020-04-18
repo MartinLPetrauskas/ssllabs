61fd1edff2cdee74fdfe6bc1bab190f668ff37d3
Martin Petrauskas <martin.petrauskas@protonmail.com>
2020-04-18 02:43:33 -0400

    Added doc strings for all functions

--- ssllabsscanner.py
+++ ssllabsscanner.py
@@ -9,12 +9,13 @@ API = 'https://api.ssllabs.com/api/v3/'
 
 
 def requestAPI(path, payload={}):
-    '''This is a helper method that takes the path to the relevant
-        API call and the user-defined payload and requests the
-        data/server test from Qualys SSL Labs.
-
-        Returns JSON formatted data'''
-
+    """
+    This is a helper function that takes the path to the relevant API call and the user defined payload and requests
+    the data/server test from Qualys SSL Labs.
+    @param path: path relevant to API call (usually 'analyze')
+    @param payload: JSON object with information about the parameters for the API call
+    @return: JSON object with results from API call
+    """
     url = API + path
 
     try:
@@ -28,6 +29,15 @@ def requestAPI(path, payload={}):
 
 
 def resultsFromCache(host, publish='off', startNew='off', fromCache='on', all='done'):
+    """
+    This function will return cached assessment reports if they are available
+    @param host: hostname which will be analyzed
+    @param publish: on/off determines if the assessment results should be published on the public results boards
+    @param startNew: on/off determines if the API will start a new assessment
+    @param fromCache: on/off determines if the API will retrieve cached assessment reports
+    @param all: on/done determines when the API returns the results
+    @return: JSON object with results from API call
+    """
     path = 'analyze'
     payload = {
                 'host': host,
@@ -41,6 +51,16 @@ def resultsFromCache(host, publish='off', startNew='off', fromCache='on', all='d
 
 
 def newScan(host, publish='off', startNew='on', all='done', ignoreMismatch='on'):
+    """
+    This function will begin a new assessment and return the results
+    @param host: hostname which will be analyzed
+    @param publish: on/off determines if the assessment results should be published on the public result boards
+    @param startNew: on/off determines if the API will start a new assessment
+    @param all: on/done determines when the API returns results
+    @param ignoreMismatch: on/off determines how the API will proceed when the server certificate does not match the
+    assessment hostname
+    @return: JSON object with results from API call
+    """
     path = 'analyze'
     payload = {
                 'host': host,
