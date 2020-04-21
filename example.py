f5e283d2b18ad58000376e559086b03d7b12d183
Martin Petrauskas <martin.petrauskas@protonmail.com>
2020-04-20 21:24:10 -0400

    Fixed comments in example.py

--- example.py
+++ example.py
@@ -3,10 +3,10 @@
 # Import the module
 import ssllabsscanner
 
-# To get results from cache, call resultsFromCache()
+# To get results from cache, call scan with fromCache parameter set to True
 cache_data = ssllabsscanner.scan("www.qualys.com", True)
 print(cache_data['endpoints'][0]['grade'])
 
-# To get fresh results, call newScan()
+# To get fresh results, call scan with fromCache parameter set to False
 new_data = ssllabsscanner.scan("www.qualys.com", False)
 print(new_data['endpoints'][0]['grade'])
